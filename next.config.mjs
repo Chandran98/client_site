8ac2460edc677e16aa22641e21c3c495e5b45c78
chandran98 <chandranjc25@gmail.com>
2024-05-16 07:51:53 +0530

    slide

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{domains:[
+        "www.investopedia.com"
+    ]}
+};
 
 export default nextConfig;
